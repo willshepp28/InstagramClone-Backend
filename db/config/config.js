6683bf32b66b5fe66805893062c2c8ec2049eedd
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2019-12-04 12:51:33 -0500

    changed db urls

--- db/config/config.js
+++ db/config/config.js
@@ -3,10 +3,10 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+    host: process.env.RDS_HOSTNAME,
     dialect: 'mysql',
     logging: console.log
   },
