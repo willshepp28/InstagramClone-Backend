88d33914514c7ec39744344833cf2eaf257dc312
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-02-17 10:50:43 -0500

    testing

--- file.js
+++ file.js
@@ -1,5 +1,8 @@
 'use strict';
 
+require('dotenv').config()
+
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -10,10 +13,26 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
+  console.log(config)
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+
+// if (process.env.DATABASE_URL) {
+//   sequelize = new Sequelize(process.env.DATABASE_URL, {
+//     use_env_variable: process.env.DATABASE_URL,
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     logging: true,
+//     dialectOptions: {
+//       ssl: true
+//     }
+//   });
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
