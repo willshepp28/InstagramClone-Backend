6c45ef55c7f77f46fe0b0ecc73cf707f3f5c1e6f
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-02-19 08:43:44 -0500

    changed reference to model Users to User in the create-post.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 const fs = require("fs")
 
-console.log("checking fs")
+
 
 
 
@@ -30,29 +30,8 @@ console.log("checking fs")
 |  Middleware
 |--------------------------------------------------------------------------
 */
-// process.on("beforeExit", code => {
-//     setTimeout(() => {
-//         console.log(`Process will exit with the code: ${code} `);
-//         console.log(process.env.DATABASE_URL || "This is not using the enviromen t varable")
-//         process.exit(code);
-//     }, 100)
-// })
-
-// process.on("uncaughtException", error => {
-//     console.log(error);
-//     console.log(process.env.DATABASE_URL)
-//     process.exit(1);
-// })
-
-// process.on('unhandledRejection', (reason, promise) => {
-//     console.log('Unhandled Rejection at:', reason.stack || reason);
-//     process.exit(1);
-//   })
 
 process.on('unhandledRejection', error => {
-    console.error(error)
-    console.log("a unhandled rejection ")
-    console.log(process.env.DATABASE_URL || "This is not using the enviromen t varable")
     process.exit(1);
 });
 
@@ -107,13 +86,11 @@ application.use("/api/friends", friendsAPI);
 
 
 
-
-
-
-// application.listen(process.env.PORT || 4000, function(){
-//     console.log("Express server listening on port %d in %s mode", this.address().port, application.settings.env);
-//   });
-
+/*
+|--------------------------------------------------------------------------
+|  START SERVER
+|--------------------------------------------------------------------------
+*/
  models.sequelize.sync().then(function() {
     application.listen(PORT, () => {
         console.log(`Server listening on port ${PORT}`)
