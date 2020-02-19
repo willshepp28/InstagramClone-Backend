afba699f58142448e2831efd8a2eb15f4eceda10
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-02-19 08:58:08 -0500

    updating app

--- db/models/post.js
+++ db/models/post.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: "User",
+        model: "Users",
         key: "id"
       }
     },
