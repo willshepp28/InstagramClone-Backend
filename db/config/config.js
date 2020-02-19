7cb375450ce3c9ff1c5fc429d50bf38b8faff525
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-02-19 09:13:27 -0500

    updating app

--- db/config/config.js
+++ db/config/config.js
@@ -18,7 +18,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL || "kdkfja;",
     dialect: 'postgres',
     protocol: 'postgres',
     logging: true,
